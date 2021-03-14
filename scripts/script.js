c117b077d865cbeb5d86265759488ac3e76f4c2c
William <swang45@uw.edu>
2021-03-15 01:05:00 +0800

    Completed

--- scripts/script.js
+++ scripts/script.js
@@ -6,42 +6,138 @@ let width = canvas.width;
 let ctx = canvas.getContext("2d");
 
 const SIZE = 10;
+let dx = 0;
+let dy = SIZE;
+let score = 0;
+
+
 function drawRect(x, y) {
-    ctx.fillStyle = "white";
-    ctx.fillRect(x, y, SIZE, SIZE);   
+  ctx.fillStyle = "white";
+  ctx.fillRect(x, y, SIZE, SIZE);
 }
 
 let food = {
-    x: 0,
-    y: 0
+  x: 0,
+  y: 0,
 };
-
-console.log(food);
 
 function addFood() {
-    let xVal = Math.floor(Math.random() * width / SIZE) * SIZE;
-    console.log(xVal);
-    
-    let yVal = Math.floor((Math.random() * width) / SIZE) * SIZE;
-    console.log(yVal);
+  let xVal = Math.floor((Math.random() * width) / SIZE) * SIZE;
 
-    food = {
-        x: xVal,
-        y: yVal
-    }
-};
+  let yVal = Math.floor((Math.random() * width) / SIZE) * SIZE;
+
+  food = {
+    x: xVal,
+    y: yVal,
+  };
+}
 
 addFood(1, 2);
-console.log(food);
-drawRect(food.x, food.y);
 
 let snake = [];
 function addSnake(xVal, yVal) {
-    snake.push({
-        x: xVal,
-        y: yVal
-    })
+  snake.push({
+    x: xVal,
+    y: yVal,
+  });
 }
 
-console.log(snake)
-addSnake(0,0)
+addSnake(0, 0);
+addSnake(SIZE, 0);
+addSnake(SIZE * 2, 0);
+
+function drawSnake() {
+  for (let i = 0; i < snake.length; i++) {
+    drawRect(snake[i].x, snake[i].y);
+  }
+}
+
+
+
+
+function create() {
+  ctx.clearRect(0, 0, width, height);
+  snake.shift();
+
+  if (foodCollide()) {
+      console.log("eat");
+      addHead();
+      addFood();
+      score += 1;
+
+      let points = document.getElementsByTagName("span")[0];
+      points.innerHTML = score;
+  }
+
+  if (selfCollide()) {
+      console.log("collide");
+      alert("you lost");
+      clearInterval(timer);
+  }
+
+  addHead();
+  drawSnake();
+
+  drawRect(food.x, food.y);
+}
+
+create();
+
+function addHead() {
+  let x = snake[snake.length - 1].x + dx;
+  let y = snake[snake.length - 1].y + dy;
+  if (x > width) {
+    x = 0;
+  } else if (y > width) {
+    y = 0;
+  } else if (x < 0) {
+    x = width;
+  } else if (y < 0) {
+    y = height;
+  }
+  addSnake(x, y);
+}
+
+let timer = setInterval(create, 100);
+window.addEventListener("keydown", keys);
+
+function keys(e) {
+  console.log(e);
+  if (e.keyCode == 38) {
+    console.log("up");
+    dx = 0;
+    dy = -SIZE;
+  } else if (e.keyCode == 39) {
+    console.log("right");
+    dx = SIZE;
+    dy = 0;
+  } else if (e.keyCode == 40) {
+    console.log("down");
+    dx = 0;
+    dy = SIZE;
+  } else if (e.keyCode == 37) {
+    console.log("left");
+    dx = -SIZE;
+    dy = 0;
+  }
+}
+
+function foodCollide() {
+    let head = snake[snake.length - 1];
+
+    if (head.x == food.x && head.y == food.y) {
+        return true
+    } else {
+    return false
+    }
+}
+
+function selfCollide() {
+    let head = snake[snake.length - 1];
+
+    for (let i = 0; i < snake.length - 1; i++) {
+        if (snake[i].x == head.x && snake[i].y == head.y) {
+            return true;
+        }
+    }
+}
